c20b041be091b4588883d872a23b6c026315cc92
Alek-DB <Alekdelbalso@gmail.com>
2025-08-04 12:21:08 -0400

    new style

---
+++ index.js
@@ -0,0 +1,34 @@
+let lastButton = null
+let lastPage = null
+
+
+window.addEventListener("load",  (e) => {
+    OnClickButton(document.getElementById("Home"))
+})
+
+const OnClickButton = (button) => {
+    if(lastButton)
+        lastButton.classList.remove("active")
+
+    button.classList.add("active")
+    lastButton = button
+
+    SwitchPage(button.innerText)
+}
+
+const SwitchPage = (page) => {
+
+    page = document.getElementById(page + "-page")
+    if(lastPage == page)
+        return
+    page.classList.add("openPage")
+    page.style.zIndex  = 2
+
+    if(lastPage){
+        lastPage.classList.remove("openPage")
+        lastPage.style.zIndex  = 1
+
+    }
+
+    lastPage = page
+}
